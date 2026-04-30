53a1eec391f42dbac460a5d7fd46c9d8bb801aa8
Matthew Wells <matthewpaulwells@gmail.com>
2026-04-30 08:13:51 -0400

    declare admin settings page

---
+++ templates/admin.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * Nextcloud Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Matthew Wells
+ * @copyright Matthew Wells 2026
+ */
+
+?>
+<div id="admin-music"></div>
